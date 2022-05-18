806fbef67a131f77742416bd9450c78e35577d1a
ctc2022 <wkn.aaa.ikeda@gmail.com>
2022-05-18 16:46:07 +0900

    池田変更2

--- app/src/main/java/com/example/toppage/kakuninn2.java
+++ app/src/main/java/com/example/toppage/kakuninn2.java
@@ -2,13 +2,32 @@ package com.example.toppage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class kakuninn2 extends AppCompatActivity {
+public class kakuninn2 extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_kakuninn2);
+        setContentView(R.layout.activity_cyuumondesu);
+
+        ((Button) findViewById(R.id.button15)).setOnClickListener((View.OnClickListener) this);
+
+    }
+
+    //onClickメソッド実装
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case (R.id.button15):
+                Intent intentStart = new Intent(getApplication(),banngou.class);
+                startActivity(intentStart);
+                break;
+
+
+        }
     }
 }
