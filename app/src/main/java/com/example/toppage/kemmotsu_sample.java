b46576cff90f5cb53c6e15d78052bc2d9eee653b
Kemmotsu-Harutaro <Harutaro.Kemmotsu@gmail.com>
2022-05-18 11:10:10 +0900

    劔持branch1回目

---
+++ app/src/main/java/com/example/toppage/kemmotsu_sample.java
@@ -0,0 +1,14 @@
+package com.example.toppage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class kemmotsu_sample extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kemmotsu_sample);
+    }
+}
