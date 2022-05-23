28fee28fa6eb766e1aaca94a94fa633d2a54e01f
Kemmotsu-Harutaro <Harutaro.Kemmotsu@gmail.com>
2022-05-23 14:58:08 +0900

    微修正

--- app/src/main/java/com/example/toppage/kakuninn.java
+++ app/src/main/java/com/example/toppage/kakuninn.java
@@ -6,8 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class kakuninn extends AppCompatActivity implements View.OnClickListener {
+
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +19,21 @@ public class kakuninn extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_kakuninn);
 
         ((Button) findViewById(R.id.credit)).setOnClickListener((View.OnClickListener) this);
+
+        Intent intentstart = getIntent();
+        //インテントに保存されたデータを取得
+        String data = intentstart.getStringExtra("result");
+
+
+        int num1 = Integer.parseInt(data);
+        int sum =num1*600;
+        TextView text = findViewById(R.id.textView_sum);
+        str = String.valueOf(sum);
+        text.setText("合計 "+str+"円");
+        TextView num = findViewById(R.id.num);
+        num.setText(data+"個");
+        num.setText(data+"個");
+
 
     }
 
@@ -24,6 +43,7 @@ public class kakuninn extends AppCompatActivity implements View.OnClickListener 
         switch (view.getId()) {
             case (R.id.credit):
                 Intent intentStart = new Intent(getApplication(),kakuninn2.class);
+                intentStart.putExtra("result2",str);
                 startActivity(intentStart);
                 break;
 
--- app/src/main/java/com/example/toppage/teisyoku.java
+++ app/src/main/java/com/example/toppage/teisyoku.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Spinner;
 
 public class teisyoku extends AppCompatActivity implements View.OnClickListener {
+
+    Spinner number;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +20,8 @@ public class teisyoku extends AppCompatActivity implements View.OnClickListener 
         setContentView(R.layout.activity_teisyoku);
 
         ((Button) findViewById(R.id.button5)).setOnClickListener(this);
+
+        number = findViewById(R.id.spinner);
 
     }
 
@@ -25,6 +31,7 @@ public class teisyoku extends AppCompatActivity implements View.OnClickListener 
         switch (view.getId()) {
             case (R.id.button5):
                 Intent intentStart = new Intent(getApplication(),kakuninn.class);
+                intentStart.putExtra("result", number.getSelectedItem().toString());
                 startActivity(intentStart);
                 break;
 
